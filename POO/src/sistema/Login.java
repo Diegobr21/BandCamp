4216fcc6294eb6529f0ebdbf55855f4c8a12b100
netotz <neto.otz@hotmail.com>
2019-04-04 18:39:21 -0600

    edit

--- POO/src/sistema/Login.java
+++ POO/src/sistema/Login.java
@@ -2,18 +2,26 @@ package sistema;
 
 import java.util.Scanner;
 
-public class Login extends Registrar {
+public class Login {
 	
 	//Metodo "ingresar", simplemente le pide al usuario que ingrese su correo electronico y contraseña, despues se verifica si estan correctas.
-	public void ingresar(Usuario cuenta) {
+	public Usuario ingresar() {
 		Scanner scanner = new Scanner(System.in);
+		
 		System.out.println("Ingresa tu correo electronico: ");
 		String correo = scanner.nextLine();
+		
 		System.out.println("Ingresa tu contraseña: ");
 		String contraseña = scanner.nextLine();
+		
 		//Validacion en base de datos
-		if (cuenta.getCor_usu().equals(correo) && cuenta.getPas_usu().equals(contraseña)) {
-			System.out.println("Correo correcto y contraseña correcta");
-		}
+		// aquí se harán los queries para comprobar que el correo y la contraseña coinciden
+//		if (cuenta.getCor_usu().equals(correo) && cuenta.getPas_usu().equals(contraseña)) {
+//			System.out.println("Correo correcto y contraseña correcta");
+//		}
+		
+		scanner.close();
+		
+		return null;
 	}
 }
--- POO/src/sistema/Registrar.java
+++ POO/src/sistema/Registrar.java
@@ -3,48 +3,57 @@ package sistema;
 import java.util.Scanner;
 
 public class Registrar {
-	static Usuario cuenta;
-	
 	//Metodo "capturar" permite al usuario registrar una nueva cuenta. Por cambiar: TODO.
 	public Usuario capturar() {
 		int id = 1;
 		int tipo = 0;
 		String tipo_usuario = "";
-		boolean bandera = true;
 		Scanner scanner = new Scanner(System.in);
+		
 		System.out.println("Ingresa tu correo electronico: ");
 		String correo = scanner.nextLine();
+		
 		System.out.println("Ingresa tu contraseña: ");
 		String contraseña = scanner.nextLine();
+		
+		boolean bandera = true;
 		//Ciclo while, permite al usuario reingresar los datos en caso de que no se ingrese un valor aceptable.
 		while (bandera) {
-		System.out.println("Ingresa una opcion: 1.- Artista 2.-Banda");
-		tipo = Integer.parseInt(scanner.nextLine());
-		//Switch, basicamente convierte el valor entero que ingreso el usuario para que se guarde como un string en otra variable.
-		switch (tipo) { 
-		case 1:
-			tipo_usuario = "Artista";
-			bandera = false;
-			break;
-		case 2:
-			tipo_usuario = "Banda";
-			bandera = false;
-			break;
-		default:
-			System.out.println("Opcion incorrecta");
+			System.out.println("Ingresa una opción: 1.- Artista 2.-Banda");
+			tipo = Integer.parseInt(scanner.nextLine());
+			
+			//Switch, basicamente convierte el valor entero que ingreso el usuario para que se guarde como un string en otra variable.
+			switch (tipo) { 
+				case 1:
+					tipo_usuario = "Artista";
+					bandera = false;
+					break;
+				case 2:
+					tipo_usuario = "Banda";
+					bandera = false;
+					break;
+				default:
+					System.out.println("Opción incorrecta.");
+					break;
 			}
 		}
+		
 		System.out.println("Ingresa tu nombre de usuario: ");
 		String nombre = scanner.nextLine();
+		
 		System.out.println("Ingresa el genero que interpretas: ");
 		String genero = scanner.nextLine();
+		
 		System.out.println("Que instrumento tocas: ");
 		String instrumento = scanner.nextLine();
+		
 		System.out.println("A que facultad perteneces: ");
 		String facultad = scanner.nextLine();
+		
+		scanner.close();
+		
 		//Se declara e instancia un nuevo objeto de tipo Usuario, despues se utilizan los metodos de acceso para obtener los datos e imprimirlos.
-		cuenta = new Usuario(id, correo, contraseña, tipo, nombre, genero, instrumento, facultad);
-		return cuenta;
+		return new Usuario(id, correo, contraseña, tipo, nombre, genero, instrumento, facultad);
 	}
 	
 	//Metodo "imprimir", hace lo que tiene que hacer, imprimir.
@@ -59,4 +68,3 @@ public class Registrar {
 		System.out.println(cuenta.getFac_usu());
 	}
 }
-
