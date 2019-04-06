175b81289da99977e25baffa0460313664ab6a2c
netotz <neto.otz@hotmail.com>
2019-04-05 20:04:06 -0600

    Agregada documentación a la clase validInt

--- POO/src/validaciones/BoolIsNumber.java
+++ POO/src/validaciones/BoolIsNumber.java
@@ -1,24 +1,22 @@
 package validaciones;
 
-public class NumeroValido {
-	public boolean isFloat(String input) {
+public class BoolIsNumber {
+	protected static boolean isFloat(String input) {
 		try {
 			Float.parseFloat(input);
 			return true;
 		}
 		catch (NumberFormatException e) {
-			System.err.println("'" + input + "' no es flotante.");
 			return false;
 		}
 	}
 	
-	public boolean isInt(String input) {
+	protected static boolean isInt(String input) {
 		try {
 			Integer.parseInt(input);
 			return true;
 		}
 		catch (NumberFormatException e) {
-			System.err.println("'" + input + "' no es entero.");
 			return false;
 		}
 	}
