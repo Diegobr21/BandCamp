5290127639ea256660a19a5c446d9d25bd1fce49
netotz <neto.otz@hotmail.com>
2019-04-09 22:42:39 -0500

    Agregados comentarios

--- Login/src/interfaces/ComboBoxes.java
+++ Login/src/interfaces/ComboBoxes.java
@@ -3,21 +3,27 @@ package interfaces;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 
+/**
+ * Contiene los comboboxes de las facultades, de los géneros y de los instrumentos.
+ */
 public class ComboBoxes {
 	JComboBox<String> cmbFacultades, cmbGeneros, cmbInstrumentos;
 	
+	/**
+	 * Define a los comboboxes, les agrega las opciones y deshabilita la edición.
+	 */
 	public ComboBoxes(){
-		cmbFacultades = new JComboBox<String>();
+		JComboBox<String> cmbFacultades = new JComboBox<String>();
 		cmbFacultades.setModel(new DefaultComboBoxModel<String>(
 				new String[] {"FIME", "FCQ", "FACPYA", "FCFM", "FARQ", "FACDYC", "FIC", "FAV", "FOD"}));
 		cmbFacultades.setEditable(false);
 		
-		cmbGeneros = new JComboBox<String>();
+		JComboBox<String> cmbGeneros = new JComboBox<String>();
 		cmbGeneros.setModel(new DefaultComboBoxModel<String>(
 				new String[] {"Rock", "Jazz", "Reggaeton", "Rap", "Metal", "Indie", "K-Pop", "Pop", "Clásica"}));
 		cmbGeneros.setEditable(false);
 				
-		cmbInstrumentos = new JComboBox<String>();
+		JComboBox<String> cmbInstrumentos = new JComboBox<String>();
 		cmbInstrumentos.setModel(new DefaultComboBoxModel<String>(
 				new String[] {"Guitarra", "Batería", "Piano", "Xilófono", "Flauta",
 						"Violín", "Trompeta", "Oboe", "Ocarina", "Bajo"}));
