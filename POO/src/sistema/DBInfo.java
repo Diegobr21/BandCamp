d3f8395844d228b0b8942d8cfdd76f2250b66ef2
netotz <neto.otz@hotmail.com>
2019-04-13 17:35:45 -0500

    agregada clase para usuario y contraseña del admin de BD, y URL conexión

---
+++ POO/src/sistema/DBInfo.java
@@ -0,0 +1,16 @@
+package sistema;
+
+/**
+ * Contiene el usuario y la contraseña del administrador de la base de datos, además del URL para la conexión.
+ */
+class DBInfo {
+	// comentado temporalmente hasta que el servidor real esté listo y solo se requiera un usuario
+	
+	// static String usuario = "DanielSal";
+	// static String password = "Avion123";
+	// static String url = "jdbc:sqlserver://localhost:1433;databaseName=Server";
+	
+	static String usuario = "sa";
+	static String password = "123";
+	static String url = "jdbc:sqlserver://localhost:1433;databaseName=Servidor";
+}
