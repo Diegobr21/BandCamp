9d81e1c5fbf4ae65927966d5751599ba15d5fcdb
netotz <neto.otz@hotmail.com>
2019-04-17 00:16:32 -0500

    actualizada clase Inicio con test a la BD y redirección a LogIn

--- POO/src/sistema/Inicio.java
+++ POO/src/sistema/Inicio.java
@@ -1,8 +1,34 @@
 package sistema;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import interfaces.UserLogin;
+
+/**
+ * Primera clase en ejecutarse al iniciar el sistema.
+ * Hace una conexión inicial a la base de datos para verificar que se pueda seguir usando el sistema.
+ * Redirige al inicio de sesión.
+ */
 class Inicio {
 	public static void main(String[] args) {
-		// aquí se instanciará la interfaz principal, que será el Log In,
-		// y de esta clase se ejecutará todo el programa.
+		
+		// test de conexión al servidor
+		try {
+			Connection connection = DriverManager.getConnection(DBInfo.url, DBInfo.usuario, DBInfo.password);
+			System.out.println("Conexión establecida");
+			connection.close();
+			
+			UserLogin logIn = new UserLogin();
+			logIn.setLocationRelativeTo(null);
+			logIn.setVisible(true);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Lo sentimos, parece que nuestros servidores fallaron.\n¡Inténtalo en unos minutos!",
+					"Error de servidor", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 }
