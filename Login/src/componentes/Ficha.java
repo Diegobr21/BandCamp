f22263b8113e2cc5d37916a380ae9180ec7a7ea8
netotz <neto.otz@hotmail.com>
2019-04-22 17:19:21 -0500

    actualizada ficha con atributos de CuentaFiltrada

--- Login/src/componentes/Ficha.java
+++ Login/src/componentes/Ficha.java
@@ -4,6 +4,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
+
+import sistema.CuentaFiltrada;
+
 import java.awt.Font;
 import java.awt.Color;
 
@@ -16,7 +19,7 @@ public class Ficha extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	public Ficha(int tipo, String nombre, String genero, String instrumento, String facultad) {
+	public Ficha(CuentaFiltrada cuentaFiltrada) {
 		setBounds(100, 100, 624, 148);
 		setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
 		setLayout(null);
@@ -26,26 +29,26 @@ public class Ficha extends JPanel {
 		lblImagen.setBounds(10, 11, 128, 128);
 		add(lblImagen);
 		
-		JLabel lblNombre = new JLabel(nombre);
+		JLabel lblNombre = new JLabel(cuentaFiltrada.getNombre());
 		lblNombre.setForeground(Color.RED);
 		lblNombre.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblNombre.setBounds(148, 11, 128, 25);
 		add(lblNombre);
 		
-		JLabel lblFacultad = new JLabel(facultad);
+		JLabel lblFacultad = new JLabel(cuentaFiltrada.getFacultad());
 		lblFacultad.setForeground(Color.DARK_GRAY);
 		lblFacultad.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		lblFacultad.setBounds(148, 125, 34, 14);
 		add(lblFacultad);
 		
-		JLabel lblGenero = new JLabel(genero);
+		JLabel lblGenero = new JLabel(cuentaFiltrada.getGenero());
 		lblGenero.setForeground(Color.BLACK);
 		lblGenero.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblGenero.setBounds(148, 69, 63, 14);
 		add(lblGenero);
 		
 		String info;
-		switch (tipo) {
+		switch (cuentaFiltrada.getTipo()) {
 		case 1:
 			info = "Busco bandas que les haga falta: ";
 			break;
@@ -57,7 +60,7 @@ public class Ficha extends JPanel {
 			break;
 		}
 		
-		JLabel lblInfo = new JLabel(info + instrumento);
+		JLabel lblInfo = new JLabel(info + cuentaFiltrada.getInstrumento());
 		lblInfo.setForeground(new Color(0, 0, 0));
 		lblInfo.setFont(new Font("Arial", Font.PLAIN, 22));
 		lblInfo.setBounds(286, 28, 280, 87);
